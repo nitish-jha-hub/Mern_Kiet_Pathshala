e1cd78e63e78b44a4eb2ffa5cd963a074559d48e
Nitish Jha <nitishkumar0914@gmail.com>
2024-05-31 10:52:50 +0530

    day 14 react

--- day14/app.js
+++ day14/app.js
@@ -1,1 +1,22 @@
-console.log("hello world");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './src/pages/common/navbar/navbar.js';
+
+const parent = document.getElementById('root');
+const root = ReactDOM.createRoot(parent);
+
+// react element
+const Heading = React.createElement('h1', {}, 'Hello World');
+
+// react component
+const Title = ()=>{
+    return <h1>Hello React 3!</h1>;
+}
+
+
+// root.render(Heading);
+// root.render(Title());
+// root.render(<Title />);
+// <Title/> = Title()
+
+root.render(<Header/>);
---
+++ day14/src/pages/common/navbar/navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <div className="header-parent-container">
+            navbar
+            </div>
+    );
+    }
+
+export default Navbar;
